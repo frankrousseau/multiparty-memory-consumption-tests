907a4f49d851f422f923359f84dbb09f4cfab942
Frank Rousseau <frank.rousseau@cozycloud.cc>
2014-07-22 23:53:01 +0200

    add comments

--- client.js
+++ client.js
@@ -2,22 +2,26 @@ var fs = require('fs');
 var path = require('path');
 var request = require('request');
 
+// Test file paths
 var file_small = './small.pdf';
 var file_medium = './medium.mp4';
 var file_big = './big.mkv';
 
+// File used for upload
 var selected_file = file_medium;
+
+
+// File upload via request library
 req = request.post('http://localhost:3000/upload', function(req, res, body) {
     console.log(body);
 });
-
 var form = req.form();
-
 form.append(
   'my_file',
   fs.createReadStream(path.join(__dirname, selected_file))
 );
 
+// Display memory consumption
 setInterval(function() {
   console.log(process.memoryUsage());
 }, 500);
--- server.js
+++ server.js
@@ -2,54 +2,59 @@ var fs = require('fs');
 var path = require('path');
 var multiparty = require('multiparty');
 var express = require('express');
-var app = express();
-  var heapdump = require('heapdump');
 
+// Set express app.
+var app = express();
+
+// Define upload dir path.
 var upload_dir = path.join(__dirname, 'uploads');
 
+// Index route.
 app.get('/', function(req, res){
   res.send('Upload server');
 });
 
+// Upload route.
 app.post('/upload', function(req, res, next){
   var form = new multiparty.Form();
 
+  // Listen for incoming part of the form.
   form.on('part', function(part) {
     var name, stream;
 
+    // It's a field, not a file.
     if (part.filename == null) {
       part.resume();
+
+    // It's a file.
     } else {
       name = part.filename;
-      console.log(name);
+
+      // Write file in upload dir.
       stream = fs.createWriteStream(path.join(upload_dir, name));
 
+      // Display sometion when file finished to upload.
       part.on('end', function(err) {
         if (err) {
           console.log(err);
-          return res.send(500, {
-            success: false
-          });
         } else {
-          clearInterval(interval);
           console.log("File " + name + " saved to disk.");
         }
       });
+
+      // Pipe the part parsing to the file writing stream.
       part.pipe(stream);
 
-      interval = setInterval(function() {
-        console.log(process.memoryUsage());
-      }, 500);
     }
   });
 
-  form.on('progress', function(bytesReceived, bytesExpected) {});
-
+  // End the request when something goes wrong.
   form.on('error', function(err) {
     console.log(err);
     next(err);
   });
 
+  // Send success code if file was successfully uploaded.
   form.on('close', function() {
     return res.send(201, {
       success: true
@@ -59,6 +64,12 @@ app.post('/upload', function(req, res, next){
   form.parse(req);
 });
 
+// Display memory usage.
+interval = setInterval(function() {
+  console.log(process.memoryUsage());
+}, 500);
+
+// Run server.
 app.listen(3000, function() {
   console.log('Upload server is listening on 3000 port...');
 });
